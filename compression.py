e72f83cf6442f6c920b0144d92170ec333904851
Milan Sood <79568472+Milan1508@users.noreply.github.com>
2023-12-27 01:45:14 +0530

    develope loop to get all jpeg and compress and save on other location

--- compression.py
+++ compression.py
@@ -3,6 +3,18 @@ from tkinter import filedialog
 from PIL import Image
 import os
 from tkinter import messagebox
+import json
+
+
+def read_config_file(file_path):
+    with open(file_path, 'r') as file:
+        config_data = json.load(file)
+    return config_data
+
+
+config_path = 'config.json'
+config_data = read_config_file(config_path)
+print(config_data)
 
 
 def append_resized_to_filename(image_path):
@@ -23,8 +35,8 @@ def process_image(image_path):
     # Get the new image path
     output_path = append_resized_to_filename(image_path)
     # Resize the image
-    width = 640
-    height = 400
+    width = config_data['width']
+    height = config_data['height']
     resize_image(image_path, output_path, width, height)
 
 
@@ -53,24 +65,11 @@ def resize_image(image_path, output_path, width, height):
     resized_image = image.resize((width, height))
     resized_image.save(output_path)
 
-
+"""
 def main():
     root = tk.Tk()
     root.withdraw()  # Hide the main window
-    # Open the folder dialog and get the selected folder path
-    # folder_path = filedialog.askdirectory()
-    # print(folder_path)
-
-    # # Example usage
-    # image_path = folder_path
-    # # output_path = append_resized_to_filename(image_path)
-    # # width = 640
-    # # height = 400
-    # # resize_image(image_path, output_path, width, height)
-    # output_path = append_resized_to_foldername(image_path)
-    # print(output_path)
-    # make_directory(output_path)
-
+    
     # Ask the user if they want to select a file or a directory
     MsgBox = messagebox.askquestion(
         'File or Directory', 'Select "Yes" for directory and "NO" for choosing jpg file', icon='question')
@@ -80,14 +79,18 @@ def main():
         directory_path = filedialog.askdirectory()
         print(directory_path)
         cycle_through_files(directory_path)
-        
+
     else:
         # Open the directory dialog and get the selected directory path
         file_path = filedialog.askopenfilename(
             filetypes=[("JPEG files", "*.jpg")])
         print(file_path)
         process_image(file_path)
+"""
 
+
+def main():
+    pass
 
 def cycle_through_files(directory_path):
     for root, dirs, files in os.walk(directory_path):
@@ -96,7 +99,6 @@ def cycle_through_files(directory_path):
                 file_path = os.path.join(root, file)
                 print(file_path)
                 process_image(file_path)
-
 
 
 if __name__ == "__main__":
